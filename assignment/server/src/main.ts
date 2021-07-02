a02f066c924a110857a11fbdb923d50bf336f321
Uriah <uharston@gmail.com>
2021-07-01 21:35:12 -0400

    Add CORS config

--- assignment/server/src/main.ts
+++ assignment/server/src/main.ts
@@ -1,8 +1,19 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const corsConfiguration = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsConfiguration);
+
+  const port = process.env.DEV_PORT || 1234;
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
