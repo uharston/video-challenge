8ae7ae7669b3cb02f6b06edef8e3ebcf15558489
Uriah <uharston@gmail.com>
2021-07-02 14:25:59 -0400

    Import Sequelize config

--- assignment/server/src/app.module.ts
+++ assignment/server/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { MediaModule } from './media/media.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+    dialect: 'mssql',
+    host: '127.0.0.1',
+    port: 1433,
+    username: 'sa',
+    password: 'MyPass@word',
+    database: 'TestDb',
+    autoLoadModels: true,
+    synchronize: true,
+  }),
+    MediaModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
